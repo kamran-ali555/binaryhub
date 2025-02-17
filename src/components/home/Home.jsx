45f4e86a4147e447c9b1a15c6c90583630fd2fe1
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-02-17 21:12:58 +0500

    final

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import Choose from "../Choose/Choose";
+import Choose from "../Choose/Choose";
 import Counter from "../Counter/Counter";
 // import HomePortfolio from "../HomePortfolio/HomePortfolio";
 import Services from "../Services/Services";
 // import MountainBanner from "../MountainBanner/MountainBanner";
 import Hero from "../Hero/Hero";
 import About from "../About/About";
-// import Header from "../Header/Header";
+import Header from "../Header/Header";
 import FAQ from "../FAQ/FAQ";
 import Testimonials from "../Testimonails/Testimonials";
 import Footer from "../Footer/Footer";
@@ -18,9 +18,7 @@ function Home() {
     <div>
       {/* <Header /> */}
      <Hero /> 
-     
-      <Explore/> 
-     
+     {/* <Explore/>  */}   
       <Services />
       <Counter />
       <HomeBlog/>
